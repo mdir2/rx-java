b6a2112622742175adff15135ba3df9fc2627859
wook <wook@gaeasoft.co.kr>
2020-09-16 16:54:04 +0900

    [1장] 1-9 함수형 인터페이스를 전달받을 때

--- src/main/java/chapter1/FunctionalTimingSample.java
+++ src/main/java/chapter1/FunctionalTimingSample.java
@@ -5,8 +5,21 @@ import io.reactivex.rxjava3.core.Flowable;
 public class FunctionalTimingSample {
 
   public static void main(String[] args) {
-    final Flowable<Long> flowable = Flowable.just(System.currentTimeMillis());
+    final Flowable<Long> justFlowable = Flowable.just(System.currentTimeMillis());
+    final Flowable<Long> flowable = Flowable.fromCallable(() -> System.currentTimeMillis());
 
     flowable.subscribe(System.out::println);
+    flowable.subscribe(System.out::println);
+    flowable.subscribe(System.out::println);
+    flowable.subscribe(System.out::println);
+    flowable.subscribe(System.out::println);
+
+    System.out.println("----");
+
+    justFlowable.subscribe(System.out::println);
+    justFlowable.subscribe(System.out::println);
+    justFlowable.subscribe(System.out::println);
+    justFlowable.subscribe(System.out::println);
+    justFlowable.subscribe(System.out::println);
   }
 }
